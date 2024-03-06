b12b1bd583ed04e11035cd2fbe5c82692df34d77
EC2 Default User <ec2-user@ip-10-0-143-156.ec2.internal>
2024-03-06 06:49:45 +0000

    updates

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css'; 
-import { Component2, NavBarHeader, MarketingFooter } from './ui-components'
+import { CardJssHomeCollection, NavBarHeader, MarketingFooter } from './ui-components'
 
 
 function App() {
 return (
   <>
     <NavBarHeader width="100%" marginBottom="20px" />
-    <Component2 />
+    <CardJssHomeCollection />
     <MarketingFooter />
   </>
 )
